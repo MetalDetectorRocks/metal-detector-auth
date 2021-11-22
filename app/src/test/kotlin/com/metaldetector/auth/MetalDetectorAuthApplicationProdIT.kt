bda76d49a24116f5f82707d5e135c13d413ac51c
hedgehog1833 <nilsdam@aol.com>
2021-11-22 17:30:41 +0100

    Integration test setup simplified

--- app/src/test/kotlin/com/metaldetector/auth/MetalDetectorAuthApplicationProdIT.kt
+++ app/src/test/kotlin/com/metaldetector/auth/MetalDetectorAuthApplicationProdIT.kt
@@ -1,19 +1,17 @@
 package com.metaldetector.auth
 
-import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.proc.SecurityContext
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
 @ActiveProfiles("prod")
-class MetalDetectorAuthApplicationProdIT : FunSpec(), WithIntegrationTestConfig {
-
-  @MockBean
-  lateinit var jwkSource: JWKSource<SecurityContext>
+@Import(IntegrationTestConfig::class)
+@TestPropertySource(locations = ["classpath:integrationtest.properties"])
+class MetalDetectorAuthApplicationProdIT : FunSpec() {
 
   init {
     test("applicationContext should load on prod profile") {
--- app/src/test/kotlin/com/metaldetector/auth/WithIntegrationTestConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.metaldetector.auth
-
-import org.springframework.test.context.TestPropertySource
-
-@TestPropertySource(locations = ["classpath:integrationtest.properties"])
-interface WithIntegrationTestConfig
