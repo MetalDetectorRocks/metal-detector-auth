31cb9ca9502564e4889d3bc8895ecaac7d471b20
Nils Dammenhayn <nilsdam@aol.com>
2022-02-12 17:45:22 +0100

    Revert "Dependency udpates"
    This reverts commit d6e2490d1b51de84c12ace5dffe5e12a5afcba08.

--- app/src/test/kotlin/rocks/metaldetector/auth/config/ExpiredTokenDeletingOAuth2AuthorizationServiceTest.kt
+++ app/src/test/kotlin/rocks/metaldetector/auth/config/ExpiredTokenDeletingOAuth2AuthorizationServiceTest.kt
@@ -1,10 +1,8 @@
 package rocks.metaldetector.auth.config
 
 import io.kotest.core.spec.style.FunSpec
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.springframework.jdbc.core.ConnectionCallback
 import org.springframework.jdbc.core.JdbcOperations
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 
@@ -14,7 +12,6 @@ class ExpiredTokenDeletingOAuth2AuthorizationServiceTest : FunSpec({
   val jdbcOperations = mockk<JdbcOperations>(relaxed = true)
 
   beforeTest {
-    every { jdbcOperations.execute(any<ConnectionCallback<Int>>()) } returns 0
     val registeredClientsRepository = mockk<RegisteredClientRepository>()
     underTest = ExpiredTokenDeletingOAuth2AuthorizationService(jdbcOperations, registeredClientsRepository)
   }
@@ -24,6 +21,6 @@ class ExpiredTokenDeletingOAuth2AuthorizationServiceTest : FunSpec({
     underTest.removeExpiredAuthorizations()
 
     // then
-    verify(exactly = 1) { jdbcOperations.update(underTest.REMOVE_AUTHORIZATION_SQL) }
+    verify(exactly =1) { jdbcOperations.update(underTest.REMOVE_AUTHORIZATION_SQL) }
   }
 })
