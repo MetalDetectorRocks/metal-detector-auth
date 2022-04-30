f73155116e06cb1fc539b249cbbaab74c1b46e8f
Nils Dammenhayn <nilsdam@aol.com>
2022-04-30 09:09:50 +0200

    Dependency Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 val javaVersion: JavaVersion = JavaVersion.VERSION_17
 val dependencyVersions: List<String> = listOf(
     "com.nimbusds:nimbus-jose-jwt:9.22",
-    "org.objenesis:objenesis:3.2"
+    "org.objenesis:objenesis:3.2",
+    "com.fasterxml.jackson:jackson-bom:2.13.2.1"
 )
 val dependencyGroupVersions: Map<String, String> = mapOf(
     "io.kotest" to libs.versions.kotest.get(),
