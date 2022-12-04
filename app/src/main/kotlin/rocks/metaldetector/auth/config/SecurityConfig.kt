20b0554e1904398ed51d574b23334b74f6588e56
Nils Dammenhayn <nilsdam@aol.com>
2022-12-04 15:43:46 +0100

    Refactor security config

--- app/src/main/kotlin/rocks/metaldetector/auth/config/SecurityConfig.kt
+++ app/src/main/kotlin/rocks/metaldetector/auth/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
@@ -14,12 +15,16 @@ class SecurityConfig {
   @Bean
   @Throws(Exception::class)
   fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-    http.authorizeHttpRequests { authorizeRequests ->
-      authorizeRequests
-          .requestMatchers("/actuator/**").permitAll()
-          .anyRequest().authenticated()
-    }
-    return http.build()
+    return http
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(STATELESS)
+        .and()
+        .authorizeHttpRequests { registry ->
+          registry
+              .requestMatchers("/actuator/**").permitAll()
+              .anyRequest().denyAll()
+        }
+        .build()
   }
 
   @Bean
