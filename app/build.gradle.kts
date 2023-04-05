324270e52719d7bc73e811a989fc355e66b5e36b
Nils Dammenhayn <nilsdam@aol.com>
2023-04-05 17:38:36 +0200

    Make better use of toml file to get dependabot work with it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,29 +30,27 @@ tasks {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib:${libs.versions.kotlin.get()}")
+  implementation(libs.kotlin)
 
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.security:spring-security-oauth2-authorization-server:${libs.versions.springSecurityAuthorization.get()}")
+  implementation(libs.springSecurityAuthorizationServer)
 
   implementation("org.flywaydb:flyway-core")
   implementation("io.micrometer:micrometer-registry-prometheus")
 
-  runtimeOnly("com.github.loki4j:loki-logback-appender:${libs.versions.lokiLogbackAppender.get()}")
-  runtimeOnly("org.postgresql:postgresql:${libs.versions.postgresql.get()}")
+  runtimeOnly(libs.lokiLogbackAppender)
+  runtimeOnly(libs.postgresql)
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("io.kotest:kotest-framework-engine-jvm:${libs.versions.kotest.get()}")
-  testImplementation("io.kotest:kotest-property-jvm:${libs.versions.kotest.get()}")
-  testImplementation("io.kotest:kotest-runner-junit5-jvm:${libs.versions.kotest.get()}")
-  testImplementation("io.kotest.extensions:kotest-extensions-spring:${libs.versions.kotestSpring.get()}")
-  testImplementation("io.mockk:mockk:${libs.versions.mockk.get()}")
+  testImplementation(libs.bundles.kotest)
+  testImplementation(libs.kotestExtensionSpring)
+  testImplementation(libs.mockk)
   testRuntimeOnly("com.h2database:h2")
 }
 
